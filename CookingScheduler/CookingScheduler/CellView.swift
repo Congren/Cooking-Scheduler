70cb958327f4f1fecb654935849e0429a43a7da7
Andrewdrew Wang <xhobobobbyx@gmail.com>
2016-11-17 01:44:34 -0500

    added overview page with functional calendar, next step is to add the popups for when the user clicks on the calendar.

---
+++ CookingScheduler/CookingScheduler/CellView.swift
@@ -0,0 +1,15 @@
+//
+//  CellView.swift
+//  CookingScheduler
+//
+//  Created by Andrew Wang on 11/17/16.
+//  Copyright © 2016 Andrew Wang. All rights reserved.
+//
+
+import UIKit
+
+import JTAppleCalendar
+class CellView: JTAppleDayCellView {
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var selectedView: UIView!
+}
--- CookingScheduler/CookingScheduler/ViewController.swift
+++ CookingScheduler/CookingScheduler/ViewController.swift
@@ -7,17 +7,81 @@
 //
 
 import UIKit
-
+import JTAppleCalendar
+extension UIColor {
+    convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0){
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(value & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
+extension ViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6,
+                                                 calendar: Calendar.current,
+                                                 generateInDates: .forAllMonths,
+                                                 generateOutDates: .tillEndOfGrid,
+                                                 firstDayOfWeek: .sunday)
+        return parameters
+    }
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        // Setup text color
+        if cellState.dateBelongsTo == .thisMonth {
+            myCustomCell.dayLabel.textColor = UIColor(colorWithHexValue: 0xECEAED)
+        } else {
+            myCustomCell.dayLabel.textColor = UIColor(colorWithHexValue: 0x574865)
+        }
+        
+        handleCellSelection(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        handleCellSelection(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        handleCellSelection(view: cell, cellState: cellState)
+    }
+    func handleCellSelection(view: JTAppleDayCellView?, cellState: CellState) {
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        if cellState.isSelected {
+            myCustomCell.selectedView.layer.cornerRadius =  25
+            myCustomCell.selectedView.isHidden = false
+        } else {
+            myCustomCell.selectedView.isHidden = true
+        }
+    }
+}
 class ViewController: UIViewController {
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView")
+        calendarView.cellInset = CGPoint(x: 0, y: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
