55d0c4b1db481d6b527fb7b3fc24a1793d1de675
Andrewdrew Wang <xhobobobbyx@gmail.com>
2016-12-08 04:05:33 -0500

    added core data + overview and ui fixes

--- CookingScheduler/Pods/Target Support Files/Pods-CookingScheduler/Pods-CookingScheduler-frameworks.sh
+++ CookingScheduler/Pods/Target Support Files/Pods-CookingScheduler/Pods-CookingScheduler-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ImageLoader/ImageLoader.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JTAppleCalendar/JTAppleCalendar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ImageLoader/ImageLoader.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JTAppleCalendar/JTAppleCalendar.framework"
 fi
